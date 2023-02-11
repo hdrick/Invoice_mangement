413e5aa2ccdd9809eedea93e3dc5f7d90694844a
hendwrick <hendwrick.gonzales@ext.essilor.ph>
2023-02-11 21:20:07 +0800

    feature(login): added login Design

---
+++ src/test/java/app/invoice/management/InvoiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.invoice.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
